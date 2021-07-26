49dc2d7167aa649cae145a300dfab6f575ba14f0
kitisaks <kitisak.spnr@gmail.com>
2021-07-27 01:11:41 +0700

    fix wrong duplicate in notfound

--- controller/error_controller.php
+++ controller/error_controller.php
@@ -6,7 +6,6 @@ class Notfound extends Plug{
   function __construct(){
     parent::__construct();
     $this->main = strtolower(get_class($this));
-    $this->view->render($this->main,"index");
   }
 
   public function index(){
