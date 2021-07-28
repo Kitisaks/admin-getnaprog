d8afd43f477947de396b5d6eb969cb9db387f174
kitisaks <kitisak.spnr@gmail.com>
2021-07-29 01:24:54 +0700

    add authentication

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,34 @@
+$(function() {
 
-$('body #sign-in').on('submit', function(e){
-  e.preventDefault();
-  let data = $('body #sign-in-form').serialize();
+  //== Dropdown
+  $('body').on('click', '.dropdown', function(){
+    var id = $(this).attr('target-id');
+    $('body #'+id).slideToggle('fast');
+  });
 
-  alert("hi");
+  //== Login form
+//   $('body').on('submit','.sign-in', function(e){
+//     e.preventDefault();
+//     var url = $(this).attr("action");
+//     var type = $(this).attr("method");
+//     var data = $(this).serialize();
+//     console.log(data);
+//     $.ajax({
+//       type: type,
+//       url: url,
+//       data: data,
+//       processData: false,
+//       success: function(param){
+//         if(param.status == true){
+//           aler
+//         }
+//       }
 
-})
+//     });
+  
+//   });
+
+});
+
+
+
